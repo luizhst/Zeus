59ff39fe182add3461e29adaeb743dd53db55f9d
luizhst <luizh_s@hotmail.com>
2018-03-19 20:42:01 -0300

    Comit Inicial da Solution

---
+++ Project_Zeus/Project_Zeus/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As Informações Gerais sobre um assembly são controladas por meio do 
+' conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Verificar os valores dos atributos do conjunto
+<Assembly: AssemblyTitle("Project_Zeus")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("Project_Zeus")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+<Assembly: Guid("23f988b9-27cd-433c-8a1e-885fbbb6bdb3")>
+
+' As informações de versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' É possível especificar todos os valores ou utilizar como padrão os Números da Versão e de Revisão 
+' usando o '*' como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Project_Zeus/Project_Zeus/Site.Master.designer.vb
@@ -0,0 +1,42 @@
+﻿'------------------------------------------------------------------------------
+' <gerado automaticamente>
+'     Este código foi gerado por uma ferramenta.
+'
+'     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+'     o código for recriado
+' </gerado automaticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class SiteMaster
+    
+    '''<summary>
+    '''Controle divMenu.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents divMenu As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+    
+    '''<summary>
+    '''Controle divLogin.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents divLogin As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+    
+    '''<summary>
+    '''Controle MainContent.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents MainContent As Global.System.Web.UI.WebControls.ContentPlaceHolder
+End Class
